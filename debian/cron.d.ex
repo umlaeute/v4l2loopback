4a84be73941d20fc5c05071f5033f0326f0b8052
gorinich.zmey <none@none>
2009-03-15 18:15:33 +0000

    removed files
    --HG--
    extra : convert_revision : svn%3Aece311c4-f1d8-11dd-a295-0b7b0935bf94/trunk%4063

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the v4l2loopback package
-#
-0 4	* * *	root	[ -x /usr/bin/v4l2loopback_maintenance ] && /usr/bin/v4l2loopback_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(v4l2loopback):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="v4l2loopback" command="/usr/bin/v4l2loopback"
