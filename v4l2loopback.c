44693d147b5beb1af95ab819123cd2af470402d6
gorinich.zmey <none@none>
2009-03-16 01:02:55 +0000

    license header add
    --HG--
    extra : convert_revision : svn%3Aece311c4-f1d8-11dd-a295-0b7b0935bf94/trunk%4064

--- v4l2loopback.c
+++ v4l2loopback.c
@@ -1,7 +1,15 @@
 /*
-YAVLD - Yet Another Video Loopback Device
-Distributed according to the GPL.
-*/
+ *      v4l2loopback.c  --  video 4 linux loopback driver
+ *
+ *      Copyright (C) 2005-2009
+ *          Vasily Levin (vasaka@gmail.com)
+ *
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 2 of the License, or
+ *      (at your option) any later version.
+ *
+ */
 #include <linux/version.h>
 #include <linux/vmalloc.h>
 #include <linux/mm.h>
--- v4l2loopback.h
+++ v4l2loopback.h
@@ -1,6 +1,13 @@
-/* 
- * File:   v4l2loopback.h
- * Author: vasaka
+/*
+ *      v4l2loopback.h  --  video 4 linux loopback driver
+ *
+ *      Copyright (C) 2005-2009
+ *          Vasily Levin (vasaka@gmail.com)
+ *
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 2 of the License, or
+ *      (at your option) any later version.
  *
  */
 
