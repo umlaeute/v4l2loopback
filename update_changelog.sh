b8c8fe7fb3db7fca11b5e0fb601561d5fcff354c
IOhannes m zmoelnig <zmoelnig@umlautO.umlaeute.mur.at>
2011-12-27 18:45:40 +0100

    try to guess the current version
    if none is specified

--- update_changelog.sh
+++ update_changelog.sh
@@ -1,15 +1,30 @@
 #!/bin/sh
 
-OLDVERSION=$1
-NEWVERSION=$2
 
-if [ "x${NEWVERSION}" = "x" ]; then
- echo "usage: $0 <LASTVERSION> <CURVERSION>" 1>&2
+getcurrentversion () {
+  dpkg-parsechangelog --count 1 -lChangeLog |  egrep "^Version:" | head -1 | cut -f2 -d' '
+}
+
+if [ "x$1" = "x" ]; then
+ echo "usage: $0 [<LASTVERSION>] <CURVERSION>" 1>&2
  exit 1
 fi
 
+if [ "x$2" = "x" ]; then
+## guess current version
+ NEWVERSION=$1
+ OLDVERSION=$(getcurrentversion)
+else
+ OLDVERSION=$1
+ NEWVERSION=$2
+fi
+
+echo "updating from $OLDVERSION to $NEWVERSION"
+
 mkdir debian
 cp ChangeLog debian/changelog
-git-dch --since ${OLDVERSION} -N ${NEWVERSION}
+git-dch --since ${OLDVERSION} -N v${NEWVERSION}
 cat debian/changelog > ChangeLog
 rm -rf debian
+
+echo "don't forget to git-tag the new version as v${NEWVERSION}"
