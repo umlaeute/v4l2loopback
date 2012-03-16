89d5ee41e6f2a235615f7979e49794e4cda1edc3
Anton Novikov <random.plant@gmail.com>
2012-03-17 00:12:24 +0400

    add examples/restarting-writer.sh

---
+++ examples/restarting-writer.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+device=${1:-/dev/video0}
+echo Using $device
+
+run_writers() {
+    for i in `seq 1 3`; do
+        sleep 1
+        gst-launch-0.10 videotestsrc horizontal-speed=1 num-buffers=30 ! v4l2sink device=$device
+    done
+}
+
+v4l2-ctl -d $device -c keep_format=1 || exit 1
+v4l2-ctl -d $device -c sustain_framerate=0 || exit 1
+gst-launch-0.10 videotestsrc num-buffers=1 ! v4l2sink device=$device || exit 1
+{
+    run_writers
+    sleep 1
+    v4l2-ctl -d $device -c sustain_framerate=1 || exit 1
+    run_writers
+} >/dev/null 2>&1 &
+gst-launch-0.10 v4l2src device=$device ! timeoverlay ! videorate force-fps="30/1" ! fpsdisplaysink
+kill $! 2>/dev/null
+wait
