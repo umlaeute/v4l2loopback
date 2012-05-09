2975e403dd0c8200bfe45fb530255dd8948fc081
Anton Novikov <random.plant@gmail.com>
2012-05-10 01:37:49 +0400

    fix buffer indices before dev->used_buffers update

--- v4l2loopback.c
+++ v4l2loopback.c
@@ -1354,6 +1354,7 @@ vidioc_reqbufs      (struct file *file,
 {
   struct v4l2_loopback_device *dev;
   struct v4l2_loopback_opener *opener;
+  int i;
   MARK();
 
   dev=v4l2loopback_getdevice(file);
@@ -1376,22 +1377,36 @@ vidioc_reqbufs      (struct file *file,
 
     if (b->count > dev->buffers_number)
       b->count = dev->buffers_number;
+
+    /* make sure that outbufs_list contains buffers from 0 to used_buffers-1
+     * actually, it will have been already populated via v4l2_loopback_init()
+     * at this point */
+    if (list_empty(&dev->outbufs_list)) {
+      for (i = 0; i < dev->used_buffers; ++i)
+        list_add_tail(&dev->buffers[i].list_head, &dev->outbufs_list);
+    }
+
+    /* also, if dev->used_buffers is going to be decreased, we should remove
+     * out-of-range buffers from outbufs_list, and fix bufpos2index mapping */
+    if (b->count < dev->used_buffers) {
+      struct v4l2l_buffer *pos, *n;
+      list_for_each_entry_safe(pos, n, &dev->outbufs_list, list_head) {
+        if (pos->buffer.index >= b->count)
+          list_del(&pos->list_head);
+      }
+
+      /* after we update dev->used_buffers, buffers in outbufs_list will
+       * correspond to dev->write_position + [0;b->count-1] range */
+      i = dev->write_position;
+      list_for_each_entry(pos, &dev->outbufs_list, list_head) {
+        dev->bufpos2index[i % b->count] = pos->buffer.index;
+        ++i;
+      }
+    }
+
     opener->buffers_number = b->count;
     if (opener->buffers_number < dev->used_buffers)
       dev->used_buffers = opener->buffers_number;
-
-    /* make sure that outbufs_list contains buffers from 0 to used_buffers-1 */
-    if (list_empty(&dev->outbufs_list)) {
-      int i;
-      for (i = 0; i < dev->used_buffers; ++i)
-        list_add_tail(&dev->buffers[i].list_head, &dev->outbufs_list);
-    } else {
-      struct v4l2l_buffer *pos, *n;
-      list_for_each_entry_safe(pos, n, &dev->outbufs_list, list_head) {
-        if (pos->buffer.index >= dev->used_buffers)
-          list_del(&pos->list_head);
-      }
-    }
     return 0;
   default:
     return -EINVAL;
