b5d76cbcf715640156c090177cd1c2cef6d72a55
IOhannes m zmölnig <zmoelnig@umlautQ.umlaeute.mur.at>
2015-04-09 18:22:47 +0200

    removed unnecessary return statement

--- v4l2loopback.c
+++ v4l2loopback.c
@@ -1227,8 +1227,6 @@ static int vidioc_s_ctrl(struct file *file, void *fh, struct v4l2_control *c)
 {
 	struct v4l2_loopback_device *dev = v4l2loopback_getdevice(file);
 	return v4l2loopback_set_ctrl(dev, c->id, c->value);
-
-	return 0;
 }
 
 /* returns set of device outputs, in our case there is only one
