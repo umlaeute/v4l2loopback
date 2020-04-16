2c82c58c5a3cabd277a603901a6b6e1fdac47411
IOhannes m zmölnig <zmoelnig@umlautS.umlaeute.mur.at>
2020-04-16 12:51:19 +0200

    use temp-var for capabilities

--- v4l2loopback.c
+++ v4l2loopback.c
@@ -671,36 +671,35 @@ static int vidioc_querycap(struct file *file, void *priv, struct v4l2_capability
 {
 	struct v4l2_loopback_device *dev = v4l2loopback_getdevice(file);
 	int devnr = ((struct v4l2loopback_private *)video_get_drvdata(dev->vdev))->devicenr;
+        __u32 capabilities = V4L2_CAP_STREAMING | V4L2_CAP_READWRITE;
 
 	strlcpy(cap->driver, "v4l2 loopback", sizeof(cap->driver));
-
 	vidioc_fill_name(cap->card, sizeof(cap->card), devnr);
-
 	snprintf(cap->bus_info, sizeof(cap->bus_info), "platform:v4l2loopback-%03d", devnr);
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(3, 1, 0)
 	/* since 3.1.0, the v4l2-core system is supposed to set the version */
 	cap->version = V4L2LOOPBACK_VERSION_CODE;
 #endif
-	cap->capabilities =
-		V4L2_CAP_STREAMING | V4L2_CAP_READWRITE;
 
 #ifdef V4L2_CAP_VIDEO_M2M
-	cap->capabilities |= V4L2_CAP_VIDEO_M2M;
+	capabilities |= V4L2_CAP_VIDEO_M2M;
 #endif /* V4L2_CAP_VIDEO_M2M */
+
 	if (dev->announce_all_caps) {
-		cap->capabilities |= V4L2_CAP_VIDEO_CAPTURE | V4L2_CAP_VIDEO_OUTPUT;
+		capabilities |= V4L2_CAP_VIDEO_CAPTURE | V4L2_CAP_VIDEO_OUTPUT;
 	} else {
 
 		if (dev->ready_for_capture) {
-			cap->capabilities |= V4L2_CAP_VIDEO_CAPTURE;
+			capabilities |= V4L2_CAP_VIDEO_CAPTURE;
 		}
 		if (dev->ready_for_output) {
-			cap->capabilities |= V4L2_CAP_VIDEO_OUTPUT;
+			capabilities |= V4L2_CAP_VIDEO_OUTPUT;
 		}
 	}
 
-	cap->device_caps = cap->capabilities;
+	cap->device_caps = cap->capabilities = capabilities;
+
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 3, 0)
 	cap->capabilities |= V4L2_CAP_DEVICE_CAPS;
 #endif
