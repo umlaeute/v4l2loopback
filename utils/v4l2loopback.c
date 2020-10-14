umlaeute/v4l2loopback

commit fbff9e84367b33e5c36f24ba80653b2393315922

handle "any" caps and set "keep_format" and "sustain_framerate"

utils/v4l2loopback.c
@@ -535,33 +535,33 @@ static int get_fps(const char *devicename)
 static int set_caps(const char *devicename, const char *capsstring)
 {
 	int result = 1;
-	int fd = -1;
+	int fd = open_videodevice(devicename, O_RDWR);
 	struct v4l2_format vid_format;
 	struct v4l2_capability vid_caps;
 	t_caps caps;
-	if (parse_caps(capsstring, &caps)) {
-		dprintf(2, "unable to parse format '%s'\n", capsstring);
-		return 1;
-	}
-	//print_caps(&caps);
 
 	/* now open up the device */
-	fd = open(devicename, O_RDWR);
-	if (fd < 0) {
-		int devnr = parse_device(devicename);
-		if (devnr >= 0) {
-			char devname[100];
-			snprintf(devname, 99, "/dev/video%d", devnr);
-			devname[99] = 0;
-			fd = open(devname, O_RDWR);
-		}
-	}
 	if (fd < 0)
 		goto done;
 
-	/* check whther this is actually a video-device */
+	if (!strncmp("any", capsstring, 4)) {
+		/* skip caps-parsing */
+	} else if (parse_caps(capsstring, &caps)) {
+		dprintf(2, "unable to parse format '%s'\n", capsstring);
+		goto done;
+	}
+	//print_caps(&caps);
+
+	/* check whether this is actually a video-device */
 	if (ioctl(fd, VIDIOC_QUERYCAP, &vid_caps) == -1) {
 		perror("VIDIOC_QUERYCAP");
+		goto done;
+	}
+
+	if (!strncmp("any", capsstring, 4)) {
+		set_control_i(fd, "keep_format", 0);
+		//set_control_i(fd, "sustain_framerate", 0);
+		result = 0;
 		goto done;
 	}
 
@@ -586,18 +586,26 @@ static int set_caps(const char *devicename, const char *capsstring)
 		goto done;
 	}
 
+	set_control_i(fd, "keep_format", 1);
+
 	/* finally, try setting the fps */
 	if (caps.fps_num && caps.fps_denom) {
 		char fps[100];
+		int didit;
 		snprintf(fps, 100, "%d/%d", caps.fps_num, caps.fps_denom);
+		didit = set_fps(devicename, fps);
+		if (!didit) {
+			set_control_i(fd, "sustain_framerate", 1);
+		}
 		close(fd);
 		fd = -1;
-		return set_fps(devicename, fps);
+		return didit;
 	}
 
 	result = 0;
 done:
-	close(fd);
+	if (fd >= 0)
+		close(fd);
 	return result;
 }
 static int get_caps(const char *devicename)

commit fd1b5642b61588c0b2c894679f66a62df1a157f1

utils: use VIDIOC_QUERYCTRL for control name resolution
VIDIOC_QUERYCTRL is sufficient for getting control id by name.
VIDIOC_QUERY_EXT_CTRL was introduced in v3.17.

Fixes: fbff9e84367b ("handle any caps and set keep_format and sustain_framerate")
Signed-off-by: You-Sheng Yang <vicamo@gmail.com>

utils/v4l2loopback.c
@@ -148,22 +148,22 @@ static unsigned int _get_control_id(int fd, const char *control)
 {
 	const size_t length = strnlen(control, 1024);
 	const unsigned next = V4L2_CTRL_FLAG_NEXT_CTRL;
-	struct v4l2_query_ext_ctrl qctrl;
+	struct v4l2_queryctrl qctrl;
 	memset(&qctrl, 0, sizeof(qctrl));
-	while (ioctl(fd, VIDIOC_QUERY_EXT_CTRL, &qctrl) == 0) {
+	while (ioctl(fd, VIDIOC_QUERYCTRL, &qctrl) == 0) {
 		if (!strncmp(qctrl.name, control, length))
 			return qctrl.id;
 		qctrl.id |= next;
 	}
 	for (int id = V4L2_CID_USER_BASE; id < V4L2_CID_LASTP1; id++) {
 		qctrl.id = id;
-		if (ioctl(fd, VIDIOC_QUERY_EXT_CTRL, &qctrl) == 0) {
+		if (ioctl(fd, VIDIOC_QUERYCTRL, &qctrl) == 0) {
 			if (!strncmp(qctrl.name, control, length))
 				return qctrl.id;
 		}
 	}
 	for (qctrl.id = V4L2_CID_PRIVATE_BASE;
-	     ioctl(fd, VIDIOC_QUERY_EXT_CTRL, &qctrl) == 0; qctrl.id++) {
+	     ioctl(fd, VIDIOC_QUERYCTRL, &qctrl) == 0; qctrl.id++) {
 		if (!strncmp(qctrl.name, control, length)) {
 			unsigned int id = qctrl.id;
 			return id;