91aa3df49aa14956a64359fe4ee2d39d933cc298
IOhannes m zmölnig <zmoelnig@umlautS.umlaeute.mur.at>
2021-01-28 10:37:46 +0100

    drop the last '#ifdef MODULE'
    we are using module_exit() so what's the point?

--- v4l2loopback.c
+++ v4l2loopback.c
@@ -2862,7 +2862,6 @@ error:
 	return err;
 }
 
-#ifdef MODULE
 static void v4l2loopback_cleanup_module(void)
 {
 	MARK();
@@ -2872,7 +2871,6 @@ static void v4l2loopback_cleanup_module(void)
 	misc_deregister(&v4l2loopback_misc);
 	dprintk("module removed\n");
 }
-#endif
 
 MODULE_ALIAS_MISCDEV(MISC_DYNAMIC_MINOR);
 
