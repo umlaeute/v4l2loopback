3e763b0bbb1aa78952c7abd249dbb8eb8238f81e
Jim Scarborough <jim.scarborough@clearspend.com>
2021-12-07 08:53:22 -0500

    Fix currentversion.sh to return the value

--- currentversion.sh
+++ currentversion.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-grep "^#define V4L2LOOPBACK_VERSION_CODE KERNEL_VERSION" v4l2loopback.c \
-| sed -e 's|^#define V4L2LOOPBACK_VERSION_CODE KERNEL_VERSION||' \
-      -e 's|^[^0-9]*||' -e 's|[^0-9]*$||' \
-      -e 's|[^0-9][^0-9]*|.|g'
+function num() {
+	grep "^#define V4L2LOOPBACK_VERSION_$1 " v4l2loopback.h | awk '{print $3}'
+}
+
+echo `num "MAJOR"`.`num "MINOR"`.`num "BUGFIX"`
