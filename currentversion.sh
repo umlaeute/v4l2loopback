7432a21fea1c7f425c41a464a4b2f5c0f783e5ed
IOhannes m zmölnig <zmoelnig@iem.at>
2022-01-19 11:01:43 +0100

    made "currentversion.sh" script POSIX conformant
    and drop useless quotes and replace `` with $()

--- currentversion.sh
+++ currentversion.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-function num() {
+num() {
 	grep "^#define V4L2LOOPBACK_VERSION_$1 " v4l2loopback.h | awk '{print $3}'
 }
 
-echo `num "MAJOR"`.`num "MINOR"`.`num "BUGFIX"`
+echo $(num MAJOR).$(num MINOR).$(num BUGFIX)
