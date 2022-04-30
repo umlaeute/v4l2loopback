9643fb0a0782c55708ac0367950a8be614116d90
Alienmaster <github@crpykng.de>
2022-04-30 16:56:54 +0000

    fixed check for non-positive values

--- v4l2loopback.c
+++ v4l2loopback.c
@@ -2552,7 +2552,7 @@ static int v4l2_loopback_add(struct v4l2_loopback_config *conf, int *ret_nr)
 	dprintk("buffer_size = %ld (=%d)\n", dev->buffer_size,
 		dev->pix_format.sizeimage);
 	err = allocate_buffers(dev);
-	if (err && dev->buffer_size)
+	if (err < 0)
 		goto out_free_handler;
 
 	init_waitqueue_head(&dev->read_event);
