6dea172ada515dc73737f56e072cce0307e762e8
Benny Baumann <BenBE@geshi.org>
2022-05-11 20:08:45 +0200

    Simplify code by using upstream video_drvdata helper
    Signed-off-by: Benny Baumann <BenBE@geshi.org>

--- v4l2loopback.c
+++ v4l2loopback.c
@@ -703,9 +703,7 @@ static struct v4l2_loopback_device *v4l2loopback_cd2dev(struct device *cd)
 
 static struct v4l2_loopback_device *v4l2loopback_getdevice(struct file *f)
 {
-	struct video_device *loopdev = video_devdata(f);
-	struct v4l2loopback_private *ptr =
-		(struct v4l2loopback_private *)video_get_drvdata(loopdev);
+	struct v4l2loopback_private *ptr = video_drvdata(f);
 	int nr = ptr->device_nr;
 
 	return idr_find(&v4l2loopback_index_idr, nr);
